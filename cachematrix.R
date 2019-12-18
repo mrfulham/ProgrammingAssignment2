02604d62020fd5534f5b37af3470d75e2e1c3519
Madeleine Fulham <mfulham@mb01.local>
2019-12-18 09:50:29 -0500

    Final commits of comment descriptions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Takes a matrix and inititalizes the functions set, get, setinverse, and getinverse and the names i and x to the makeCacheMatrix() environment. Then uses cacheSolve() function to check if the listed function getinverse is cached. If not, it assigns the listed function setinverse to the inverse of the passed matrix. 
+## Takes a matrix and inititalizes the functions set, get, setinverse, and getinverse and the names i and x to the makeCacheMatrix() environment. 
+## Then uses cacheSolve() function to check if the listed function getinverse is cached for the matrix in question. 
+## If not, it passes the inverse of the matrix to the listed function setinverse. 
 
 
-## Write a short comment describing this function
+
 ## This function creates a list of functions (set, get, setinverse, and getinverse) and the objects i and x and stores them within the parent environment (i.e. makeCacheMatrix() environment). 
 ## The makeCacheMatrix is assigned to the global environment.
 
@@ -18,11 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
 # This function takes a function of makeCacheMatrix (e.g. cacheSolve(makeCacheMatrix(matrix(c(2, 4, -3, -7), nrow = 2)))).
 # It is essential that the matrix set in the makeCacheMatrix is invertible (i.e. the determinant is not 0).
 # This function checks the parent environment (i.e. makeCacheMatrix()) for the inverse of x via getinverse(); if it cached, it returns the cached inverse.
-# If it is not cached, it creates the inverse with the setinverse() function stored within makeCacheMatrix.
+# If it is not cached, it caches the inverse with the setinverse() function stored within makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
